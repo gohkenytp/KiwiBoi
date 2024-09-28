6399e72dcfe64f35c328359d30b8070155afdb60
gohkenytp <34868610+gohkenytp@users.noreply.github.com>
2024-09-28 15:00:36 -0400

    Update KiwiBoi.java

--- src/main/java/codyhuh/kiwiboi/KiwiBoi.java
+++ src/main/java/codyhuh/kiwiboi/KiwiBoi.java
@@ -3,26 +3,23 @@ package codyhuh.kiwiboi;
 import codyhuh.kiwiboi.common.entities.Kiwi;
 import codyhuh.kiwiboi.registry.KBEntities;
 import codyhuh.kiwiboi.registry.KBItems;
-import codyhuh.kiwiboi.registry.KBPaintings;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
 
 @Mod(KiwiBoi.MOD_ID)
 public class KiwiBoi {
     public static final String MOD_ID = "kiwiboi";
 
-    public KiwiBoi() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        IEventBus forgeBus = MinecraftForge.EVENT_BUS;
+    public KiwiBoi(ModContainer container, IEventBus bus) {
+        IEventBus forgeBus = NeoForge.EVENT_BUS;
 
         KBItems.ITEMS.register(bus);
         KBEntities.ENTITIES.register(bus);
-        KBPaintings.PAINTINGS.register(bus);
 
         bus.addListener(this::registerAttributes);
         bus.addListener(this::populateTabs);
@@ -42,5 +39,3 @@ public class KiwiBoi {
     }
 
 }
-
-
