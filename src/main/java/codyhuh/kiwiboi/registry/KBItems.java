8e1b708eed1352dc94a2e7dcd8eb3d84eaa4f39c
gohkenytp <34868610+gohkenytp@users.noreply.github.com>
2024-09-28 14:54:53 -0400

    Update KBItems.java

--- src/main/java/codyhuh/kiwiboi/registry/KBItems.java
+++ src/main/java/codyhuh/kiwiboi/registry/KBItems.java
@@ -1,16 +1,17 @@
 package codyhuh.kiwiboi.registry;
 
 import codyhuh.kiwiboi.KiwiBoi;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.common.ForgeSpawnEggItem;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.neoforge.common.DeferredSpawnEggItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
 
 public class KBItems {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, KiwiBoi.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(BuiltInRegistries.ITEM, KiwiBoi.MOD_ID);
 
-    public static final RegistryObject<Item> KIWI_SPAWN_EGG = ITEMS.register("kiwi_spawn_egg", () -> new ForgeSpawnEggItem(KBEntities.KIWI, 0x814a31, 0x543628, new Item.Properties()));
-    public static final RegistryObject<Item> KIWICAKE = ITEMS.register("kiwicake", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().fast().saturationMod(0.4F).nutrition(6).build())));
+    public static final Supplier<Item> KIWI_SPAWN_EGG = ITEMS.register("kiwi_spawn_egg", () -> new DeferredSpawnEggItem(KBEntities.KIWI, 0x814a31, 0x543628, new Item.Properties()));
+    public static final Supplier<Item> KIWICAKE = ITEMS.register("kiwicake", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().fast().saturationModifier(0.4F).nutrition(6).build())));
 }
